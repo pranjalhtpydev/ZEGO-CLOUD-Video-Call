efa306ae839450367c15dd258684e5c1ecb91a96
Pranjal Sharma <139797941+pranjalhtpydev@users.noreply.github.com>
2024-03-22 01:28:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+}); 
